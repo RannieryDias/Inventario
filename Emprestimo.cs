1a855b6278177e38adec642ddcb6e1e3a4b9b781
RannieryDias <ranniery.dias@live.com>
2020-03-03 15:28:28 -0300

    Modificação geral nas classes

---
+++ Emprestimo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventario_POLI
+{
+    class Emprestimo
+    {
+        public Item item;
+        public string LocalOrigem
+        {
+            set;
+            get;
+        }
+
+        public string LocalDestino
+        {
+            set;
+            get;
+        }
+
+        public void Emprestar(Item item, string LocalOrigem, string LocalDestino)
+        {
+            item = new Item();
+            this.item = item;
+            this.LocalOrigem = LocalOrigem;
+            this.LocalDestino = LocalDestino;
+            this.item.Emprestado = true;
+        }
+    }
+}
---
+++ Item.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventario_POLI
+{
+    class Item
+    {
+        private string _nome;
+        public string Nome
+        {
+            set
+            {
+                if (value == null)
+                {
+
+                }
+                else
+                {
+                    _nome = value;
+                }
+            }
+            get
+            {
+                return _nome;
+            }
+        }
+
+        public string _marca;
+        public string Marca
+        {
+            set
+            {
+                if (value == null)
+                {
+
+                }
+                else
+                {
+                    _marca = value;
+                }
+            }
+            get
+            {
+                return _marca;
+            }
+        }
+
+        private int _protocolo;
+        public int Protocolo
+        {
+            set
+            {
+                if (value == 0 || value < 0)
+                {
+
+                }
+                else
+                {
+                    _protocolo = value;
+                }
+            }
+            get
+            {
+                return _protocolo;
+            }
+        }
+
+        public string LocalAtual
+        {
+            set;
+            get;
+        }
+
+        public bool Emprestado
+        {
+            set;
+            get;
+        }
+
+    }
+}
